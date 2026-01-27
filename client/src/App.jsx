4a4173fd4ac7c2dd981a8613ee4e3f92470850a2
JBot <jbot@ycmjason.com>
2026-01-27 23:45:02 +0000

    fix: add left margin for sidebar on desktop

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -64,7 +64,7 @@ function AppContent() {
         onToggleTheme={toggleTheme}
       />
       
-      <div className="flex-1 flex flex-col min-h-screen">
+      <div className="flex-1 flex flex-col min-h-screen lg:ml-72">
         <Routes>
           <Route 
             path="/" 
